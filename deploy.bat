ce063661f6e8dae581d9d2950dbbf4ba4c84eeee
zengdw <309606298@qq.com>
2024-11-13 15:10:21 +0800

    add file

--- deploy.bat
+++ deploy.bat
@@ -17,6 +17,6 @@ echo *************clean remote folder*****
 call ssh zdw@vps.zengdw.com -p 2222 "cd /data/blog; rm -rf *"
 
 echo *************copy file***************
-scp -r ./public/* -p 2222 zdw@vps.zengdw.com:/data/blog/
+scp  -p 2222 -r ./public/* zdw@vps.zengdw.com:/data/blog/
 
 pause 
